49a981e10ed1178a4862cff58ac6527b4b094dd7
Your Name <abdullahiraziah@gmail.com>
2024-02-21 19:35:53 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(){
+    name("Hello Tom")
+    modulus(7,3)
+    fact("I am a henna artist")
+
+}
+fun name(names:String){
+    var name= "Hello" + "" + "Tom"
+    println(name)
+
+}
+fun modulus(num1:Int, num2:Int){
+    var modulus= num1%num2
+    println(modulus)
+
+}
+fun fact(sentence:String){
+    var fact = sentence
+    println(fact)
+
+}
